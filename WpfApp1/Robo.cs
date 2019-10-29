67adb57992130be04c582e6d40060659c6dcdbee
Andrej Suty <andrej.suty@iter40.at>
2019-10-29 23:49:23 +0100

    Kappa 1

---
+++ WpfApp1/Robo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1 {
+    class Robo {
+        public Robo() { 
+        }
+
+        public int add( int a, int b) {
+            return a + b;
+        }
+    }
+}
