65dc40d3beeb45094aae7dbff8279e4fc46b2e06
I Am <xmalikr@stuba.sk>
2019-10-30 12:12:56 +0100

    Spracovanie dat, class-y pre zaznamy a pozicie.

---
+++ WpfApp1/HenkelJobPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace WpfApp1
+{
+    [XmlRoot(ElementName = "henkeljobpositions")]
+    public class HenkelJobPosition
+    {
+        [XmlElement(ElementName = "position")]
+        public Position[] Position { get; set; }
+    }
+}
---
+++ WpfApp1/Position.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Xml.Serialization;
+
+namespace WpfApp1
+{
+    public class Position
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "role")]
+        public string Role { get; set; }
+
+        [XmlElement(ElementName = "skills")]
+        public string Skills { get; set; }
+
+        [XmlElement(ElementName = "keywords")]
+        public string Keywords { get; set; }
+
+        public int Score { get; set; }
+
+        public ArrayList StringList { get; set; }
+    }
+}
