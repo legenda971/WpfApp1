31fe6c2931ab09be1775aaf6b555c9e00e511b50
Andrej <legenda971@gmail.com>
2019-10-30 10:23:18 +0100

    File Browser && console

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    ///
+    public partial class MainWindow : Window {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Console.WriteLine("Otvorene okno");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*pdf";
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
     }
+
 }
