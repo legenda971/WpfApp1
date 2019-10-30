b6b5ae3790f8d6c7be3cbba80373ae7a259491b3
I Am <xmalikr@stuba.sk>
2019-10-30 12:37:19 +0100

    Zmena spracovania dat  v class DataProcessing.

--- WpfApp1/DataProcessing.cs
+++ WpfApp1/DataProcessing.cs
@@ -1,33 +1,30 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Xml.Serialization;
 
 namespace WpfApp1
 {
     public class DataProcessing
     {
-        public HenkelJobPosition recordsProcessing(String path) { 
+        public HenkelJobPosition recordsProcessing(String path)
+        {
             using (FileStream file = new FileStream(@path, FileMode.Open))
             {
                 XmlSerializer serial = new XmlSerializer(typeof(HenkelJobPosition));
-                return ((HenkelJobPosition) serial.Deserialize(file));
+                return ((HenkelJobPosition)serial.Deserialize(file));
             }
         }
 
         public void stringsProcessing(HenkelJobPosition records)
         {
-            ArrayList stringList = new ArrayList();
-            ArrayList wordList = new ArrayList();
-
             foreach (Position pos in records.Position)
             {
                 string phrase = pos.Keywords;
                 phrase = phrase.Trim(new Char[] { '\n' });
 
-                string[] words = pos.Keywords.Split("; ");
+                string[] words = phrase.Split(new string[] { "; ", ", " }, StringSplitOptions.None);
+
                 foreach (String word in words)
                     pos.StringList.Add(word);
             }
